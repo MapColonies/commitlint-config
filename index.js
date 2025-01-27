02c684b16daea98b1374d8a0049692cfd4a45f98
Ofer <12687466+CptSchnitz@users.noreply.github.com>
2025-01-27 09:24:23 +0000

    style: prettier

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['deps', 'devdeps', 'helm', 'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'type-enum': [
+      2,
+      'always',
+      ['deps', 'devdeps', 'helm', 'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
   },
 };
